0811f91cc1a2ad1ebdcb50244f3dab3400d1cf26
Alejandro Chacón M <chaconmelgarejo@gmail.com>
2017-08-11 15:17:45 -0400

    errores de sintaxis exec

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
-#! /bin/bash#
+#! /bin/bash
 docker exec librenms_nms_1 sh -c "cd /opt/librenms; php /opt/librenms/build-base.php"
 docker exec librenms_nms_1 php /opt/librenms/adduser.php admin admin 10 admin@midominio.org
 docker exec -it librenms_nms_1 /opt/librenms/snmp-scan.php -r 192.168.0.0/16
